b966d3a5ed7d927b12cc2aa58a0f0ada706c8bc0
Ken Kovar <ken.kovar@yahoo.com>
2013-02-24 09:51:38 -0600

    Initial commit after adding some files

---
+++ SimplePhotoGallery.Tests/Utilities/ImageResizerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimplePhotoGallery.Utilities;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace SimplePhotoGallery.Tests.Utilities
+{
+    [TestClass]
+    public class ImageResizerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //set a working directory
+            string WorkingDirectory = @".\";
+
+            //create a image object containing a verticel photograph
+            Image imgPhotoVert = Image.FromFile(WorkingDirectory + @"\imageresize_vert.jpg");
+            Image imgPhotoHoriz = Image.FromFile(WorkingDirectory + @"\imageresize_horiz.jpg");
+            Image imgPhoto = null;
+
+            imgPhoto = ImageResize.ScaleByPercent(imgPhotoVert, 50);
+            imgPhoto.Save(WorkingDirectory + @"\images\imageresize_1.jpg", ImageFormat.Jpeg);
+            imgPhoto.Dispose();
+
+            imgPhoto = ImageResize.ConstrainProportions(imgPhotoVert, 200, ImageResize.Dimensions.Width);
+            imgPhoto.Save(WorkingDirectory + @"\images\imageresize_2.jpg", ImageFormat.Jpeg);
+            imgPhoto.Dispose();
+
+            imgPhoto = ImageResize.FixedSize(imgPhotoVert, 200, 200);
+            imgPhoto.Save(WorkingDirectory + @"\images\imageresize_3.jpg", ImageFormat.Jpeg);
+            imgPhoto.Dispose();
+
+            imgPhoto = ImageResize.Crop(imgPhotoVert, 200, 200, ImageResize.AnchorPosition.Center);
+            imgPhoto.Save(WorkingDirectory + @"\images\imageresize_4.jpg", ImageFormat.Jpeg);
+            imgPhoto.Dispose();
+
+            imgPhoto = ImageResize.Crop(imgPhotoHoriz, 200, 200, ImageResize.AnchorPosition.Center);
+            imgPhoto.Save(WorkingDirectory + @"\images\imageresize_5.jpg", ImageFormat.Jpeg);
+            imgPhoto.Dispose();
+
+
+        }
+    }
+}
---
+++ SimplePhotoGallery/Controllers/UploadPicturesController.cs
@@ -0,0 +1,53 @@
+﻿using SimplePhotoGallery.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimplePhotoGallery.Controllers
+{
+    public class UploadPicturesController : Controller
+    {
+        //
+        // GET: /UploadPictures/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    [AcceptVerbs(HttpVerbs.Post)]
+    public ActionResult Save()
+    {
+        foreach (string file in Request.Files)
+        {
+            var hpf = Request.Files[file]  ;
+            if (hpf.ContentLength == 0)
+                continue;
+            string savedFileName = Path.Combine(
+               AppDomain.CurrentDomain.BaseDirectory + @"\images",
+               Path.GetFileName(hpf.FileName));
+            hpf.SaveAs(savedFileName);
+
+            //create a image object 
+            Image imgPhotoOriginal = Image.FromFile(savedFileName);
+            Image imgPhoto = null;
+
+            imgPhoto = ImageResize.ScaleByPercent(imgPhotoOriginal, 20);
+            imgPhoto.Save(AppDomain.CurrentDomain.BaseDirectory + @"\images\thumbs\" + hpf.FileName, ImageFormat.Jpeg);
+            imgPhoto.Dispose();
+
+
+        }
+        return View();
+    }
+
+
+
+
+    }
+}
